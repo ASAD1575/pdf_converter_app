cdd94b3b6ce023494341bd04e4f34c6a10e8e6e9
ASAD1575 <masadsubhani8@gmail.com>
2025-08-01 08:05:28 +0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,3 @@
-variable "s3_bucket_name" {
-  description = "The name of the S3 bucket to store Lambda code"
-  type        = string
-  default     = "pdflambdabucket1575"
-}
-
-variable "source_code_path" {
-  description = "The path to the source code for the Lambda function"
-  type        = string
-  default     = "pdf_converter_FastAPI_app/"
+variable "source_code_hash" {
+  type = string
 }

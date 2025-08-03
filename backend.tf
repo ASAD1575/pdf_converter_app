26ca8e7cfc45ae092b4dbe78f26250cb1ec50505
ASAD1575 <masadsubhani8@gmail.com>
2025-08-04 02:31:53 +0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "masadsubhani8s3bucket204896"
+    bucket         = "pdfappbackend"
     key            = "terraform.tfstate"
-    region         = "eu-north-1"
+    region         = "us-east-1"
     dynamodb_table = "terraform-state-lock-table"
     
   }

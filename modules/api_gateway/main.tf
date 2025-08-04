9bd2a9a31f3a6c5453edb46e7c498aabc08672eb
ASAD1575 <masadsubhani8@gmail.com>
2025-08-05 00:35:10 +0500

    Update main.tf

--- modules/api_gateway/main.tf
+++ modules/api_gateway/main.tf
@@ -201,7 +201,7 @@ resource "aws_api_gateway_deployment" "main_deployment" {
 resource "aws_api_gateway_stage" "prod_stage" {
   deployment_id = aws_api_gateway_deployment.main_deployment.id
   rest_api_id   = aws_api_gateway_rest_api.main_api.id
-  stage_name    = "prod"
+  stage_name    = "/"
 
   tags = {
     Name = "${var.api_name}-prod-stage"

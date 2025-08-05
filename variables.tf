ca498e8e713d553c5f835bd7c95abdfa85081d21
ASAD1575 <masadsubhani8@gmail.com>
2025-08-05 19:52:31 +0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,12 @@
+variable "s3_key_app" {
+  type = string
+}
+
 variable "source_code_hash_app" {
+  type = string
+}
+
+variable "s3_key_layer" {
   type = string
 }
 
@@ -6,16 +14,16 @@ variable "source_code_hash_layer" {
   type = string
 }
 
+variable "s3_key_layer_libreoffice" {
+  type = string
+}
+
+variable "source_code_hash_layer_libreoffice" {
+  type = string
+}
+
 variable "region" {
   type = string
   default = "us-east-1"
-}
-
-variable "s3_key_app" {
-  type = string
-}
-
-variable "s3_key_layer" {
-  type = string
 }
 

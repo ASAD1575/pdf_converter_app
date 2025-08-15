6dcc3711d99c43c95bb4e3819e82d6905af7c6d3
ASAD1575 <masadsubhani8@gmail.com>
2025-08-15 20:50:18 +0500

    file updated

--- modules/efs/outputs.tf
+++ modules/efs/outputs.tf
@@ -6,7 +6,7 @@ output "name" {
 
 output "LibreOfficeEFS_access_point" {
   description = "The access point ID for the EFS file system."
-  value       = aws_efs_access_point.libreoffice_ap.arn
+  value       = aws_efs_access_point.libreoffice_ap.id
   
 }
 
@@ -14,4 +14,9 @@ output "file_system_dns" {
   description = "The DNS name of the EFS file system."
   value       = aws_efs_file_system.libreoffice_fs.dns_name
   
+}
+
+output "efs_access_point_arn" {
+  value = aws_efs_access_point.libreoffice_ap.arn
+  description = "The ARN of the EFS access point."
 }

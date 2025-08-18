f94c72de5546f8aba29098a66aaf1ceb67b80d0c
ASAD1575 <masadsubhani8@gmail.com>
2025-08-18 19:56:35 +0500

    pipeline updated

--- outputs.tf
+++ outputs.tf
@@ -80,3 +80,9 @@ output "efs_access_point_arn" {
   value = module.efs.efs_access_point_arn
   description = "The ARN of the EFS access point."
 }
+
+output "efs_file_system_id" {
+  value = module.efs.efs_file_system_id
+  description = "The ID of the EFS file system."
+  
+}
